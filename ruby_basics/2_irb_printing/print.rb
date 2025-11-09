f9d324ad11c73b8b088c80c3d6636b240e971107
Anthony S.J. Henry <30806798+AnthonySJHenry@users.noreply.github.com>
2025-11-09 06:27:28 -0500

    fix syntax

--- ruby_basics/2_irb_printing/print.rb
+++ ruby_basics/2_irb_printing/print.rb
@@ -1,5 +1,10 @@
-irb(main):001:0> print "Hey, this isn't so bad!"
+print "Hey, this isn't so bad!"
 
 until gets.chomp == "yes" do
   puts "Do you like Ruby? (you have to say yes): "
+end
+
+
+5.times do |number|
+  puts "Alternative fact number #{number += 1}"
 end
